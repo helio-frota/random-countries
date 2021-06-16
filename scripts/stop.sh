43f2f61fa2174ab0d7bf009b6d7ebca586a25199
Helio Frota <00hf11@gmail.com>
2021-06-16 15:21:49 -0300

    test only

---
+++ scripts/stop.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#: Title       : Kafka Stop script
+#: Date        : 2021-06-16
+#: Author      : "Helio Frota" <00hf11 at gmail dot com>
+#: Version     : 0.0.1
+#: Description : Stops kafka in 'zookeeper-less' mode
+
+./bin/kafka-server-stop.sh
